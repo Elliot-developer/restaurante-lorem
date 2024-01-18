9b4f0de28f4e63d1be01b88abcbbfc9e397c6e41
gabrielTessarolo <gabriel.tessarolo.h@gmail.com>
2024-01-18 14:28:50 -0300

    adicionado painel dinamico

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 
+let imgAtual = 0;
 
-const updatePainel = () =>{
+const updatePainel = () => {
+    let fileNames = ["/imgs/PainelPrincipal.jpg", "/imgs/FreteGratis.png", "/imgs/descontos50.png", "/imgs/loremBanner.png", "/imgs/ofertaEspecial.png"];
+    const imagemPainel1 = document.getElementById('imagemPainel1');
+    const imagemPainel2 = document.getElementById('imagemPainel2');
+    imgAtual += 1;
+    if(imgAtual>=fileNames.length) imgAtual = 0;
+    imagemPainel1.src = fileNames[imgAtual];
+    imagemPainel2.src = fileNames[[0, imgAtual+1][Number(imgAtual+1<fileNames.length)]];
 
+    console.log('cuzin');
 }
+
+setInterval(updatePainel, 3000);
